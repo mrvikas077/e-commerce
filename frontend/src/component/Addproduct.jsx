104685309cb9f87959dd821f2e494854c03e8403
mrvikas077 <mr7vikas@gmail.com>
2024-07-09 11:46:32 +0530

    new msg

--- frontend/src/component/Addproduct.jsx
+++ frontend/src/component/Addproduct.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom'
 
 export default function Addproduct() {
     let navigation = useNavigate()
-
+// heloo vikas
     // let [data,setData] = useState({
     //     productBrand: "",
     //     productPrice: "",
